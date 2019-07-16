30125a736627567ecb75ed82720bbedfc081c558
David Kirk <david.is.kirk@gmail.com>
2019-07-16 14:14:23 -0400

    added a form to site

--- django-drf-react-quickstart/project/frontend/src/components/App.js
+++ django-drf-react-quickstart/project/frontend/src/components/App.js
@@ -1,9 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import DataProvider from "./DataProvider";
-import Table from "./Table";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import DataProvider from './DataProvider';
+import Table from './Table';
+import Form from './Form';
 const App = () => (
-  <DataProvider endpoint="api/lead/" render={data => <Table data={data} />} />
-);
-const wrapper = document.getElementById("app");
-wrapper ? ReactDOM.render(<App />, wrapper) : null;
+  <React.Fragment>
+    <DataProvider endpoint='api/lead/' render={data => <Table data={data} />} />
+    <Form endpoint='api/lead/' />
+  </React.Fragment>
+)
+const wrapper = document.getElementById('app')
+wrapper ? ReactDOM.render(<App />, wrapper) : null
